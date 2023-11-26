579f7c43a34f176df85f94eef09c02caf9c88257
Neel <nkachalia1@gmail.com>
2023-11-26 15:00:00 -0500

    create express error handling

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+const debug = require('debug');
+
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -5,6 +7,8 @@ const logger = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
+
+require('./models/User');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
@@ -42,4 +46,27 @@ app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
-module.exports = app;
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+
+  const serverErrorLogger = debug('backend:error');
+
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
+
+
+
+  module.exports = app;
